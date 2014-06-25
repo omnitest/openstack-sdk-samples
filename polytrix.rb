d89f2b2cb64225e9dd8470852743c7f20775508d
Max Lincoln <max@devopsy.com>
2014-06-25 16:30:36 -0400

    Add gophercloud submodule

--- polytrix.rb
+++ polytrix.rb
@@ -6,20 +6,6 @@ require 'helpers/pacto_helper'
 require 'pacto/extensions/matchers'
 require 'pacto/extensions/loaders/simple_loader'
 
-# Quick hack for a demo, need a better way to detect language later
-def infer_lang(sdk_name)
-  {
-    'fog' => 'ruby',
-    'gophercloud' => 'go',
-    'jclouds' => 'java',
-    'openstack.net' => 'c', # should be c#, but see https://github.com/tripit/slate/issues/29
-    'php-opencloud' => 'php',
-    'pyrax' => 'python',
-    'pkgcloud' => 'javascript'
-  }[sdk_name]
-end
-
-
 Dir['spec/polytrix/runners/middleware/*.rb'].each do |middleware|
   file = middleware.gsub('spec/', '').gsub('.rb','')
   require file
@@ -27,13 +13,14 @@ end
 
 Polytrix.configure do |c|
   c.test_manifest = 'polytrix_tests.yml'
-  Dir['sdks/*'].each { |sdk|
-    next if sdk =~ /jclouds/
-    next if sdk =~ /php-opencloud/
-    c.implementor sdk
-  }
+  c.implementor 'sdks/fog'
+  c.implementor 'sdks/gophercloud/acceptance'
   c.implementor 'sdks/jclouds/rackspace'
+  c.implementor 'sdks/openstack.net'
   c.implementor 'sdks/php-opencloud/samples'
+  c.implementor 'sdks/pkgcloud'
+  c.implementor 'sdks/pyrax'
+
   # Mimic isn't really ready
   # c.middleware.insert 0, Polytrix::Runners::Middleware::Mimic, {}
   c.middleware.insert 0, Polytrix::Runners::Middleware::Pacto, {}
