42b3d70d8a97601dd83e14c597d55d7c5f13739a
Max Lincoln <max@devopsy.com>
2014-11-10 13:08:35 -0500

    Update product names in expectations

--- tests/polytrix/compute_validators.rb
+++ tests/polytrix/compute_validators.rb
@@ -2,5 +2,5 @@ require 'polytrix'
 
 Polytrix.validate 'Create server', suite: 'Compute', scenario: 'create server' do |challenge|
   detected_services = challenge.spy_data[:pacto][:detected_services]
-  expect(detected_services).to include 'os-compute-2 - Create server'
+  expect(detected_services).to include 'Cloud Compute - Create server'
 end
--- tests/polytrix/identity_validators.rb
+++ tests/polytrix/identity_validators.rb
@@ -2,5 +2,5 @@ require 'polytrix'
 
 Polytrix.validate 'Identity Authenticate Token', suite: 'Identity', scenario: 'authenticate token' do |challenge|
   detected_services = challenge.spy_data[:pacto][:detected_services]
-  expect(detected_services).to include 'identity-admin - Authenticate for Admin API'
+  expect(detected_services).to include 'Cloud Identity - Authenticate for Admin API'
 end
