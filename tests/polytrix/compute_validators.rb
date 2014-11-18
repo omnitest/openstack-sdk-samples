e58443967b4f8382aaf6040add29fc93c1bcd301
Max Lincoln <max@devopsy.com>
2014-11-18 17:03:32 -0500

    Fix Compute -> Servers in expectations

--- tests/polytrix/compute_validators.rb
+++ tests/polytrix/compute_validators.rb
@@ -2,5 +2,5 @@ require 'polytrix'
 
 Polytrix.validate 'Create server', suite: 'Compute', scenario: 'create server' do |challenge|
   detected_services = challenge.spy_data[:pacto][:detected_services]
-  expect(detected_services).to include 'Cloud Compute - Create server'
+  expect(detected_services).to include('Cloud Servers - Create server'), "The 'Cloud servers - Create server' service was not called"
 end
