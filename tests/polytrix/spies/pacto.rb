8216133bed9c72dab72ff4f9e1c2894a41f1c424
Max Lincoln <max@devopsy.com>
2014-12-05 12:44:17 -0500

    no pry

--- tests/polytrix/spies/pacto.rb
+++ tests/polytrix/spies/pacto.rb
@@ -119,7 +119,6 @@ module Polytrix
         object.body = object.body.force_encoding('utf-8') # JSON/YAML should be UTF-8 encoded
 
         if object.content_type.match(/image|octet-stream|audio/) || object.body.bytesize >= 15000
-          require 'pry'; binding.pry
           chksum = Digest::MD5.hexdigest(object.body)
           object.body = "(Omitted large or binary data (md5: #{chksum})"
         end
